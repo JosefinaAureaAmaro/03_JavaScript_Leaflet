f8aa1330e0ebac3f1cc2f6785c8b3897c79a1ce6
JosefinaAureaAmaro <46851546+JosefinaAureaAmaro@users.noreply.github.com>
2019-11-03 01:46:04 -0400

    to rename repo & set local path

---
+++ static/js/logic_3.js
@@ -0,0 +1,139 @@
+// Step 1: USGS API
+// ===================================
+var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson" + 
+                    "&starttime=2018-08-01&endtime=2018-08-31&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+
+
+// Step 2: Gather data into d3 JSON format 
+// ===================================
+var  myMap, longLat;
+
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+    console.log(`properties: ${Object.keys(data)}`); 
+    console.log(`mag: ${data.features[0].properties.mag}`);
+
+    var test = data.features[0].geometry.coordinates.slice(0,2);
+
+    console.log(`coordinates: ${test}`); 
+ 
+  createFeatures(data);
+
+  });
+
+function createFeatures(earthquakeData) {
+
+    console.log(earthquakeData);
+
+    // This function returns the style data for each of the earthquakes we plot on
+    // the map. We pass the magnitude of the earthquake into two separate functions
+    // to calculate the color and radius.
+    function styleInfo(feature) {
+        // console.log(feature)
+    return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: getColor(feature.properties.mag),
+            color: "#000000",
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
+
+    // This function determines the color of the marker based on the magnitude of the earthquake.
+    function getColor(magnitude) {
+        switch (true) {
+        case magnitude > 5:
+            return "#ea2c2c";
+        case magnitude > 4:
+            return "#ea822c";
+        case magnitude > 3:
+            return "#ee9c00";
+        case magnitude > 2:
+            return "#eecc00";
+        case magnitude > 1:
+            return "#d4ee00";
+        default:
+            return "#98ee00";
+        }
+    }
+
+    // This function determines the radius of the earthquake marker based on its magnitude.
+    // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
+    function getRadius(magnitude) {
+    if (magnitude === 0) {
+        return 1;
+    }
+
+    return magnitude * 4;
+    }
+
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng);
+      },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+    // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      }
+
+  });
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+
+// Step 5: Create Map based on layers and features
+// ===================================
+function createMap(earthquakes) {
+
+    // Define streetmap and darkmap layers
+    var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.streets",
+      accessToken: API_KEY
+    });
+  
+    var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.dark",
+      accessToken: API_KEY
+    });
+  
+    // Define a baseMaps object to hold our base layers
+    var baseMaps = {
+      "Street Map": streetmap,
+      "Dark Map": darkmap
+    };
+  
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+      Earthquakes: earthquakes
+    };
+  
+    // Create our map, giving it the streetmap and earthquakes layers to display on load
+    myMap = L.map("map", {
+      center: [
+        37.09, -95.71
+      ],
+      zoom: 5,
+      layers: [streetmap, earthquakes]
+    });
+  
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+  }
